45930ab01c2a6c14aea489c9a6750dbd2611d732
prabhakarBU <psharma3@binghamton.com>
2024-12-02 10:54:47 -0600

    Set up GitHub Actions workflow for deploying Python script to Vultr

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-source ~/anaconda3/bin/activate your_conda_env
-cd /home/dev/your_project_directory
-python main.py
+# Step 3.4: Activate the environment
+conda activate devenv
+conda install pip
+cd /home/dev/testing-cassandra-remote
+# Step 3.5: Install the required dependencies (from requirements.txt)
+if [ -f "testing-cassandra-remote/requirements.txt" ]; then
+pip3 install -r testing-cassandra-remote/requirements.txt
+fi
+
+# Step 3.6: Run your Python script or entry point (e.g., main.py)
+python3 /home/dev/testing-cassandra-remote/main.py
