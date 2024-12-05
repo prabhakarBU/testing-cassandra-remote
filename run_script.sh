d16f26eaef09dd54f8e9ff912d9e859b67ed2547
prabhakarBU <psharma3@binghamton.com>
2024-12-05 10:42:26 -0600

    Set up GitHub Actions workflow for deploying Python script with cassandra with datasnake-test-env to Vultr

--- run_script.sh
+++ run_script.sh
@@ -2,21 +2,46 @@
 # Step 3.3: Create the Anaconda environment if it doesn't exist
 if ! conda info --envs | grep -q 'datasnake-test-env'; then
 echo 'Creating Anaconda environment...'
-conda create --n datasnake-test-env python=3.8 -y
+conda create --name datasnake-test-env python=3.8 -y
 else
 echo 'Environment already exists.'
 fi
 
 # Step 3.4: Activate the environment
+conda init
 conda activate datasnake-test-env
 # conda install pip
 pip3 install cassandra-driver
 cd /home/dev/testing-cassandra-remote
 # Step 3.5: Install the required dependencies (from requirements.txt)
-if [ -f "testing-cassandra-remote/requirements.txt" ]; then
-pip3 install -r testing-cassandra-remote/requirements.txt
+if [ -f "requirements.txt" ]; then
+pip3 install -r requirements.txt
 fi
-cassandra -e "source db-script.cql"
+
+# Verify Polars installation
+if ! python3 -c "import polars" &>/dev/null; then
+    echo "Polars is not installed. Installing it explicitly..."
+    pip3 install polars
+fi
+
+CASSANDRA_HOST="127.0.0.1"
+USERNAME="cassandra"
+PASSWORD="cassandra"
+CQL_FILE="/home/dev/testing-cassandra-remote/db-script.cql"
+cqlsh $CASSANDRA_HOST -u $USERNAME -p $PASSWORD -f $CQL_FILE
+
+
+# Step 3.7: S3 Bucket Creation (Dynamic)
+project_name="testing-cassandra-remote"
+bucket_name="s3://$project_name-bucket"
+
+# Check if the bucket exists
+if ! s3cmd ls | grep -q "$bucket_name"; then
+    echo "Bucket does not exist. Creating bucket: $bucket_name"
+    s3cmd mb "$bucket_name"
+else
+    echo "Bucket $bucket_name already exists."
+fi
 
 # Step 3.6: Run your Python script or entry point (e.g., main.py)
-python3 /home/dev/testing-cassandra-remote/main.py
+python3 /home/dev/testing-cassandra-remote/test-insert-cassandra.py
